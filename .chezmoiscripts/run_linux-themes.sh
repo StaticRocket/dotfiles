599522384364e3285ed7497c78afaac3533f2320
Randolph Sapp <rs@ti.com>
2025-07-31 15:16:30 -0500

    gtk: move theme applications to script
    Move theme applications to a chezmoi script since there are additional
    options that can only be toggled through gsettings now, and we should
    only need to apply these when updating configs.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

---
+++ .chezmoiscripts/run_linux-themes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# GTK theme garbage
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+gsettings set org.gnome.desktop.interface cursor-theme capitaine-cursors-light
+gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark-solid
